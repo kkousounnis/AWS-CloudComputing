004b624306f8366014054017da59c352ade4fee3
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-11-03 03:23:10 -0800

    Update main file.

--- aws-terraform-jenkins-docker-ansible/myLab/main.tf
+++ aws-terraform-jenkins-docker-ansible/myLab/main.tf
@@ -105,16 +105,82 @@ resource "aws_route_table_association" "mylab5" {
 
 
 resource "aws_instance" "linux" {
+  ami                         = var.ami_ubuntu
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.mylab3.id]
+  subnet_id                   = aws_subnet.mylab1.id
+  associate_public_ip_address = true
+  user_data                   = file("./InstallJenkins.sh")
+
+  tags = {
+    "Name" = "linux-ubuntu-instance"
+  }
+
+}
+
+resource "aws_instance" "AnsibleController" {
   ami                         = var.ami
   instance_type               = var.instance_type
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.mylab3.id]
   subnet_id                   = aws_subnet.mylab1.id
   associate_public_ip_address = true
+  user_data                   = file("./InstallAnsibleCN.sh")
 
   tags = {
-    "Name" = "linux-instance"
+    "Name" = "Ansible-ControlNode"
   }
 
 }
 
+# Create/Launch an AWS EC2 Instance (Ansible Managed Node1) to host Apache Tomcat Server
+
+resource "aws_instance" "AnsibleManagedNode1" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.mylab3.id]
+  subnet_id                   = aws_subnet.mylab1.id
+  associate_public_ip_address = true
+  user_data                   = file("./AnsibleManagedNode.sh")
+
+  tags = {
+    "Name" = "Ansible-Managed-Node-Apache-Tomcat"
+  }
+
+}
+
+# Create/Launch an AWS EC2 Instance (Docker Host)
+
+resource "aws_instance" "dockerHost" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.mylab3.id]
+  subnet_id                   = aws_subnet.mylab1.id
+  associate_public_ip_address = true
+  user_data                   = file("./Docker.sh")
+
+  tags = {
+    "Name" = "ansible-managed-node-apache-docker-host"
+  }
+
+}
+
+# Create/Launch an AWS EC2 Instance to host Sonatype Nexus
+
+resource "aws_instance" "nexusHost" {
+  ami                         = var.ami
+  instance_type               = var.instance_type_for_nexus
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.mylab3.id]
+  subnet_id                   = aws_subnet.mylab1.id
+  associate_public_ip_address = true
+  user_data                   = file("./InstallNexus.sh")
+
+  tags = {
+    "Name" = "nexus-server"
+  }
+
+}
